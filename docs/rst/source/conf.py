8cf653b91ac33f88583a1f86c5c269c6b911a3b4
nilott <nicholas.ilott@kennedy.ox.ac.uk>
2018-10-31 14:10:51 +0000

    added cgatcore to conf.py as test

--- docs/rst/source/conf.py
+++ docs/rst/source/conf.py
@@ -15,6 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../../pipelines'))
+sys.path.insert(0, os.path.abspath('../../../../cgat-developers-v0/cgat-core/cgatcore'))
 
 
 # -- Project information -----------------------------------------------------
