3a34ea30cc7ad4903d0abcf0cf05bc6c4719e9a1
nilott <nicholas.ilott@kennedy.ox.ac.uk>
2020-10-30 10:29:08 +0000

    testing adding commandline interface

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-from distutils.core import setup
+import sysconfig
+import sys
+import os
+import subprocess
+import re
+from setuptools import setup, find_packages
 
 setup(
-    name="NGSKit",
-    version="0.1",
-    packages=["pipelines",]
-    )
-    
+    # package information
+    name='ocms 16S',
+    version="0.0",
+    description='ocms : Oxford Centre for Microbiome Studies apps',
+    author='Nicholas Ilott, Sandi Yen, Jethro Johnson',
+    license="MIT",
+    platforms=["any"],
+    keywords="microbiome, metagenomics, genomics",
+    packages=find_packages() + find_packages("./pipelines"),
+    entry_points={
+        'console_scripts': ['ocms = ocms:main']
+    }
+)
