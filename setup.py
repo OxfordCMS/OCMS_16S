3c2c7bf6c63df02dc19bb3111f5ba66d475efbd4
nilott <nicholas.ilott@kennedy.ox.ac.uk>
2020-10-31 20:52:56 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,16 @@ from setuptools import setup, find_packages
 setup(
     # package information
     name='ocms_16S',
-    version="0.0",
-    description='ocms : Oxford Centre for Microbiome Studies apps',
+    version="0.0.1",
+    description='OCMS_16S : Oxford Centre for Microbiome Studies pipelines for 16S amplicon sequencing analysis',
     author='Nicholas Ilott, Sandi Yen, Jethro Johnson',
     license="MIT",
     platforms=["any"],
     keywords="microbiome, metagenomics, genomics",
+    url="https://github.com/OxfordCMS/OCMS_16S",
     packages=find_packages("./") + find_packages("./pipelines"),
     entry_points={
         'console_scripts': ['ocmsflow = ocms_16S.ocmsflow:main']
     }
 )
+
