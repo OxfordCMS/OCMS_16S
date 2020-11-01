f3829d146b635b7cb52076182de04088e89b83a4
nilott <nicholas.ilott@kennedy.ox.ac.uk>
2020-11-01 01:09:00 +0000

    testing install

--- install_ocms_16S.sh
+++ install_ocms_16S.sh
@@ -1,17 +1,20 @@
 
 # r dependencies
 echo "installing R dependencies"
-conda install -c r r-ggplot2 \
+conda install -c condo-forge python=3.7.1
+conda install -c conda-forge r-base=3.6.1
+conda install -c condo-forge r-rcppparallel
+conda install -c condo-forge r-optparse
+conda install -c conda-forge r-ggplot2 \
       r-plotly \
       r-gridextra \
       r-dplyr \
       r-reshape \
       r-gplots \
       r-data.table \
-      r-optparse \
 
 echo "installing bioconda packages"
-conda install -c bioconda r-rcppparallel bioconductor-dada2 cgat-core
+conda install -c cgat-core bioconductor-dada2
 
 echo "installing OCMS 16S"
 python setup.py install
