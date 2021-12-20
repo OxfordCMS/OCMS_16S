d9748f38d949df381140a8f19a55f1340945c9d8
Nicholas Ilott <nicholas.ilott@kennedy.ox.ac.uk>
2021-12-20 11:01:39 +0000

    added requirements etc to setup.py

--- setup.py
+++ setup.py
@@ -16,9 +16,7 @@ setup(
     keywords="microbiome, metagenomics, genomics",
     url="https://github.com/OxfordCMS/OCMS_16S",
     download_url="https://github.com/OxfordCMS/OCMS_16S/archive/refs/tags/v0.0.1.tar.gz",
-    install_requires=["numpy==1.19.5",
-                      "pandas",
-                      "cgatcore"],
+    install_requires=["cgatcore"],
     packages=find_packages("./") + find_packages("./ocms16S/"),
     entry_points={
         'console_scripts': ['ocms_16s = ocms16S.ocms_16s:main']
